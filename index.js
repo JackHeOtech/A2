df394ad82773ff61daeb533ae8d1f8cfaa533243
JackHeOtech <jack.he@ontariotechu.net>
2024-11-03 16:22:32 -0500

    index change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+const express = require("express")
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -12,3 +13,24 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//Defining the routes to the different view pages and setting the view engine as ejs
+
+app.set("view engine", "ejs");
+
+app.get("/index", (req,res) => {
+  res.render("index", {title: "Home"}) //res.render works by loading the first parameter (view) which is your web page, locals is a variable inside the webpage you want to pass to the view
+})
+
+app.get("/about", (req,res) => {
+  res.render("about", {title: "About Me"})
+})
+
+app.get("/projects", (req,res) => {
+  res.render("projects", {title: "Projects"})
+})
+
+app.get("/contact", (req,res) => {
+  res.render("contact", {title: "Contact Me"})
+})
+
